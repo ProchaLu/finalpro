bf8bc6fe7d28ce4411edb2ac19c130fc3f56c3e9
Petzlc <chlodwig.petzl@hotmail.com>
2024-07-10 16:19:39 +0100

    Add polls to database

--- database/polls.ts
+++ database/polls.ts
@@ -1,11 +1,63 @@
 import { cache } from 'react';
+import { Poll } from '../migrations/00001-createTablePolls';
+import { sql } from './connect';
 
-export type Poll = {
-  id: number;
-  title: string;
-  description: string;
-  isPrivate: boolean;
-  userId: number;
-};
+// export type Poll = {
+//   id: number;
+//   title: string;
+//   description: string;
+//   isPrivate: boolean;
+//   userId: number;
+// };
 
-export const createPoll = cache();
+export type PollWithId = Poll & { id: number };
+
+export const createPoll = cache(
+  async (title: string, description: string, userId: number) => {
+    const [poll] = await sql<PollWithId[]>`
+      INSERT INTO
+        polls (
+          title,
+          description,
+          is_private,
+          user_id
+        )
+      VALUES
+        (
+          ${title},
+          ${description},
+          FALSE,
+          ${userId}
+        )
+      RETURNING
+        id,
+        title,
+        description,
+        is_private,
+        user_id
+    `;
+    return poll;
+  },
+);
+
+export const getPollById = cache(async (id: number) => {
+  const [poll] = await sql<PollWithId[]>`
+    SELECT
+      *
+    FROM
+      polls
+    WHERE
+      id = ${id}
+  `;
+  return poll;
+});
+
+export const getAllPolls = cache(async () => {
+  const polls = await sql<PollWithId[]>`
+    SELECT
+      *
+    FROM
+      polls
+  `;
+  return polls;
+});
